26234987d9e9a7609086ebd3e66d5626e7dc5f5f
jxk <1057234791@qq.com>
2018-08-28 10:53:45 +0800

    常用全局函数

---
+++ Core/function.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/28 0028
+ * Time: 10:39
+ */
+
+//print_r
+function p( $arr ){
+    echo '<pre>';
+    print_r($arr);
+    echo '</pre>';
+}
+
+
+//var_dump
+function v( $arr ){
+    echo '<pre>';
+    var_dump($arr);
+    echo '</pre>';
+}
+
+
+//var_export
+function ve( $arr ){
+    echo '<pre>';
+    var_export($arr);
+    echo '</pre>';
+}
